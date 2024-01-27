291376db3bdf8e9a05c885b83a957beeb4330d60
bonysameer <connectbsameer@gmail.com>
2024-01-27 16:27:30 +0530

    first commit

---
+++ src/pages/world-hexagon-map.js
@@ -0,0 +1,387 @@
+import styles from "./world-hexagon-map.module.css";
+import arow from "./asset/arow.svg";
+import arrow from "./asset/arrow.svg";
+import hexmap from "./asset/hex-map.svg";
+import icon from "./asset/icon.svg";
+import indicator from "./asset/indicator.svg";
+import option from "./asset/option.svg";
+import profile from "./asset/profile.svg";
+import shape from "./asset/shape.svg";
+import wstflogo from "./asset/wstflogo-1.svg";
+
+const WorldHexagonMap = () => {
+  return (
+    <div className={styles.worldHexagonMap}>
+      <div className={styles.widgetLSizeTimelineCha}>
+        <div className={styles.info}>
+          <div className={styles.value}>
+            <div className={styles.value1}>
+              <div className={styles.title}>Sales Figures</div>
+              <div className={styles.value2}>$10,430</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.graph}>
+          <div className={styles.chart}>
+            <div className={styles.column} />
+            <div className={styles.column1} />
+            <div className={styles.column2} />
+            <div className={styles.column3} />
+            <div className={styles.column4} />
+            <div className={styles.column5} />
+            <div className={styles.column4} />
+            <div className={styles.column7} />
+            <div className={styles.column8} />
+            <div className={styles.column3} />
+            <div className={styles.column3} />
+            <div className={styles.column7} />
+            <div className={styles.column3} />
+            <div className={styles.column13} />
+            <div className={styles.column14} />
+            <div className={styles.column15} />
+            <div className={styles.column16} />
+            <div className={styles.column14} />
+            <div className={styles.column18} />
+            <div className={styles.column19} />
+            <div className={styles.column20} />
+            <div className={styles.column21} />
+            <div className={styles.column22} />
+            <div className={styles.column23} />
+            <div className={styles.column24} />
+            <div className={styles.column18} />
+            <div className={styles.column26} />
+            <div className={styles.column26} />
+            <div className={styles.column28} />
+            <div className={styles.column26} />
+            <div className={styles.column16} />
+            <div className={styles.column31} />
+            <div className={styles.column14} />
+            <div className={styles.column14} />
+            <div className={styles.column16} />
+            <div className={styles.column14} />
+            <div className={styles.column13} />
+            <div className={styles.column14} />
+            <div className={styles.column15} />
+            <div className={styles.column16} />
+            <div className={styles.column14} />
+            <div className={styles.column14} />
+            <div className={styles.column42} />
+            <div className={styles.column31} />
+            <div className={styles.column15} />
+            <div className={styles.column15} />
+            <div className={styles.column} />
+            <div className={styles.column1} />
+            <div className={styles.column2} />
+            <div className={styles.column3} />
+            <div className={styles.column50} />
+            <div className={styles.column50} />
+            <div className={styles.column52} />
+            <div className={styles.column50} />
+            <div className={styles.column54} />
+            <div className={styles.column55} />
+            <div className={styles.column15} />
+            <div className={styles.column15} />
+            <div className={styles.column54} />
+            <div className={styles.column15} />
+            <div className={styles.column60} />
+            <div className={styles.column15} />
+            <div className={styles.column13} />
+            <div className={styles.column54} />
+            <div className={styles.column54} />
+            <div className={styles.column54} />
+            <div className={styles.column54} />
+            <div className={styles.column15} />
+            <div className={styles.column68} />
+            <div className={styles.column68} />
+            <div className={styles.column70} />
+            <div className={styles.column71} />
+          </div>
+          <div className={styles.line}>
+            <div className={styles.bg} />
+            <div className={styles.cian} />
+            <div className={styles.yellow} />
+          </div>
+        </div>
+      </div>
+      <div className={styles.navigationBar}>
+        <div className={styles.background} />
+        <div className={styles.search}>
+          <div className={styles.input} />
+          <img className={styles.icon} alt="" src={icon} />
+        </div>
+        <div className={styles.tab} />
+        <div className={styles.menu}>
+          <div className={styles.label}>Statistics</div>
+          <div className={styles.label1}>Overview</div>
+          <div className={styles.label}>Dashboard</div>
+          <div className={styles.label}>Analytics</div>
+        </div>
+        <img className={styles.profileIcon} alt="" src={profile} />
+        <img className={styles.optionIcon} alt="" src={option} />
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.background} />
+        <b className={styles.orionDataVisualisation}>
+          Orion data visualisation
+        </b>
+        <b className={styles.b}>2019</b>
+      </div>
+      <div className={styles.sideIndicators}>
+        <div className={styles.informerInfoWithIcon}>
+          <img className={styles.icon1} alt="" src={icon} />
+          <div className={styles.info1}>
+            <div className={styles.title1}>Total earning</div>
+            <div className={styles.amount}>
+              <div className={styles.name}>540,549</div>
+              <div className={styles.indicator}>
+                <img className={styles.upIcon} alt="" src="/up.svg" />
+                <div className={styles.div}>145</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.informerInfoWithIcon}>
+          <img className={styles.icon1} alt="" src={icon} />
+          <div className={styles.info1}>
+            <div className={styles.title1}>Sales</div>
+            <div className={styles.amount}>
+              <div className={styles.name}>1,205,677</div>
+              <div className={styles.indicator}>
+                <img className={styles.upIcon} alt="" src="/up.svg" />
+                <div className={styles.div}>145</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.informerInfoWithIcon}>
+          <img className={styles.icon1} alt="" src={icon} />
+          <div className={styles.info1}>
+            <div className={styles.title1}>Purchase</div>
+            <div className={styles.amount}>
+              <div className={styles.name}>48,430,039</div>
+              <div className={styles.indicator}>
+                <img className={styles.upIcon} alt="" src="/up.svg" />
+                <div className={styles.div}>145</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.widgetMSizeBigCircleC}>
+        <div className={styles.chart1}>
+          <div className={styles.percent}>
+            <span className={styles.span}>27</span>%
+          </div>
+          <div className={styles.background1} />
+          <div className={styles.progress} />
+        </div>
+        <div className={styles.info4}>
+          <div className={styles.amount3}>92,980</div>
+          <div className={styles.detail}>Active users</div>
+        </div>
+      </div>
+      <div className={styles.widgetMSizeBigCircleC1}>
+        <div className={styles.chart1}>
+          <div className={styles.percent}>
+            <span className={styles.span}>67</span>%
+          </div>
+          <div className={styles.background1} />
+          <div className={styles.progress1} />
+        </div>
+        <div className={styles.info4}>
+          <div className={styles.amount3}>22,652</div>
+          <div className={styles.detail}>New users</div>
+        </div>
+      </div>
+      <img className={styles.hexMapIcon} alt="" src={hexmap} />
+      <div className={styles.mapPinPinWithName}>
+        <div className={styles.tooltip}>
+          {/* <img className={styles.indicatorIcon} alt="" src={indicator} /> */}
+          <div className={styles.info1}>
+            {/* <div className={styles.title4}>Chicago</div> */}
+            {/* <div className={styles.amount5}>98,320,300</div> */}
+          </div>
+        </div>
+        <img className={styles.arrowIcon} alt="" src={arrow} />
+      </div>
+      <div className={styles.mapPinPinWithName1}>
+        <div className={styles.tooltip}>
+          {/* <img className={styles.indicatorIcon} alt="" src={indicator} /> */}
+          <div className={styles.info1}>
+            {/* <div className={styles.title4}>Manaus</div> */}
+            {/* <div className={styles.amount5}>12,320,300</div> */}
+          </div>
+        </div>
+        <img className={styles.arrowIcon} alt="" src={arrow} />
+      </div>
+      <div className={styles.mapPinPinWithName2}>
+        <div className={styles.tooltip}>
+          <img className={styles.indicatorIcon} alt="" src={indicator} />
+          <div className={styles.info1}>
+            <div className={styles.title4}>Berlin</div>
+            <div className={styles.amount5}>76,541,106</div>
+          </div>
+        </div>
+        <img className={styles.arrowIcon} alt="" src={arrow} />
+      </div>
+      <div className={styles.mapPinPinWithName3}>
+        <div className={styles.tooltip}>
+          <img className={styles.indicatorIcon} alt="" src={indicator} />
+          <div className={styles.info1}>
+            <div className={styles.title4}>Giza</div>
+            <div className={styles.amount5}>10,547,980</div>
+          </div>
+        </div>
+        <img className={styles.arrowIcon} alt="" src={arrow} />
+      </div>
+      <div className={styles.mapPinPinWithName4}>
+        <div className={styles.tooltip}>
+          <img className={styles.indicatorIcon} alt="" src={indicator} />
+          <div className={styles.info1}>
+            <div className={styles.title4}>Shanghai</div>
+            <div className={styles.amount5}>239,570,110</div>
+          </div>
+        </div>
+        <img className={styles.arrowIcon} alt="" src={arrow} />
+      </div>
+      <div className={styles.mapPinPinWithName5}>
+        <div className={styles.tooltip}>
+          <img className={styles.indicatorIcon} alt="" src={indicator} />
+          <div className={styles.info1}>
+            <div className={styles.title4}>Queensland</div>
+            <div className={styles.amount5}>6,097,321</div>
+          </div>
+        </div>
+        <img className={styles.arrowIcon} alt="" src={arrow} />
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.background} />
+        <b className={styles.orionDataVisualisation}>
+          Orion data visualisation
+        </b>
+        <b className={styles.b}>bony sameer@2024</b>
+      </div>
+      <div className={styles.amount11}>2,431,340</div>
+      <div className={styles.title10}>
+        <div className={styles.name}>All users</div>
+        <div className={styles.detail2}>
+          <b className={styles.detail3}>Detail</b>
+          <img className={styles.arowIcon} alt="" src={arow} />
+        </div>
+      </div>
+      <div className={styles.widgetLSizeGlobalStati}>
+        <div className={styles.header}>
+          <div className={styles.title11}>Sales Figures</div>
+          <div className={styles.legend}>
+            <div className={styles.shape} />
+            <div className={styles.marketingSales}>Sales</div>
+          </div>
+          <div className={styles.legend}>
+            <div className={styles.shape1} />
+            <div className={styles.marketingSales}>Users</div>
+          </div>
+          <div className={styles.legend}>
+            <div className={styles.shape2} />
+            <div className={styles.marketingSales}>Product</div>
+          </div>
+          <div className={styles.legend}>
+            <div className={styles.shape3} />
+            <div className={styles.marketingSales}>Team</div>
+          </div>
+        </div>
+        <div className={styles.graph1}>
+          <div className={styles.chart3}>
+            <div className={styles.yAxis}>
+              <div className={styles.k}>1k</div>
+              <div className={styles.div3}>0</div>
+              <div className={styles.div4}>200</div>
+              <div className={styles.div5}>400</div>
+              <div className={styles.div6}>600</div>
+              <div className={styles.div7}>800</div>
+            </div>
+            <div className={styles.xAxis}>
+              <div className={styles.dec}>
+                <div className={styles.name1}>Dec</div>
+                <img className={styles.shapeIcon} alt="" src={shape} />
+              </div>
+              <div className={styles.nov}>
+                <div className={styles.name2}>Nov</div>
+                <img className={styles.shapeIcon} alt="" src={shape} />
+              </div>
+              <div className={styles.oct}>
+                <div className={styles.name3}>Oct</div>
+                <img className={styles.shapeIcon2} alt="" src={shape} />
+              </div>
+              <div className={styles.sep}>
+                <div className={styles.name4}>Sep</div>
+                <img className={styles.shapeIcon3} alt="" src={shape} />
+              </div>
+              <div className={styles.aug}>
+                <div className={styles.name5}>Aug</div>
+                <img className={styles.shapeIcon} alt="" src={shape} />
+              </div>
+              <div className={styles.jul}>
+                <div className={styles.name6}>Jul</div>
+                <img className={styles.shapeIcon5} alt="" src={shape} />
+              </div>
+              <div className={styles.jun}>
+                <div className={styles.name7}>Jun</div>
+                <img className={styles.shapeIcon6} alt="" src={shape} />
+              </div>
+              <div className={styles.may}>
+                <div className={styles.name8}>May</div>
+                <img className={styles.shapeIcon7} alt="" src={shape} />
+              </div>
+              <div className={styles.apr}>
+                <div className={styles.name9}>Apr</div>
+                <img className={styles.shapeIcon8} alt="" src={shape} />
+              </div>
+              <div className={styles.mar}>
+                <div className={styles.name10}>Mar</div>
+                <img className={styles.shapeIcon9} alt="" src={shape} />
+              </div>
+              <div className={styles.feb}>
+                <div className={styles.name11}>Feb</div>
+                <img className={styles.shapeIcon10} alt="" src={shape} />
+              </div>
+              <div className={styles.jan}>
+                <div className={styles.name11}>Jan</div>
+                <img className={styles.shapeIcon11} alt="" src={shape} />
+              </div>
+            </div>
+          </div>
+          <div className={styles.data}>
+            <div className={styles.bubble} />
+            <div className={styles.bubble1} />
+            <div className={styles.bubble2} />
+            <div className={styles.bubble3} />
+            <div className={styles.bubble4} />
+            <div className={styles.bubble5} />
+            <div className={styles.bubble6} />
+            <div className={styles.bubble7} />
+            <div className={styles.bubble8} />
+            <div className={styles.bubble9} />
+            <div className={styles.bubble10} />
+            <div className={styles.bubble11}>
+              <div className={styles.bubble12} />
+              <div className={styles.value6}>$27632</div>
+            </div>
+            <div className={styles.bubble13}>
+              <div className={styles.bubble14} />
+              <div className={styles.value7}>$27632</div>
+            </div>
+            <div className={styles.bubble15}>
+              <div className={styles.bubble16} />
+              <div className={styles.value8}>$27632</div>
+              <div className={styles.text}>August</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.name13}>WASSERSTOFF</div>
+      <img className={styles.wstfLogo1Icon} alt="" src={wstflogo} />
+    </div>
+  );
+};
+
+export default WorldHexagonMap;
